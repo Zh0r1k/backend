dd21d39af8e954daab69a82d8c770c8a97109b16
SweetyOWO <36s@mail.ru>
2024-05-26 20:44:51 +0300

    Update game.js

--- models/game.js
+++ models/game.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const userModel = require('./users');
+const usersModel = require('./users');
 const categoryModel = require('./category');
 
 const gameShema = new mongoose.Schema({
@@ -25,7 +25,7 @@ const gameShema = new mongoose.Schema({
     },
     users: [{
         type: mongoose.Schema.Types.ObjectId,
-        ref: userModel
+        ref: usersModel
     }],
     categories: [{
         type: mongoose.Schema.Types.ObjectId,
